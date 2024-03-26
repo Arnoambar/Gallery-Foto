6178a24394b9b5abcf3c5e9bf42306001e2e86eb
Arnoambar <164984717+Arnoambar@users.noreply.github.com>
2024-03-26 12:12:11 +0700

    Add files via upload

---
+++ register.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Galeri Foto</title>
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+    <link rel="shortcut icon" href="asset/img/yphk.png">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid" style="background-color: yellow;">
+    <a class="navbar-brand" href="index.php">Website Galeri Foto</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+        
+      </div>
+      <a href="register.php" class="btn btn-ouline-primary m-1">Registrasi</a>
+      <a href="Login.php" class="btn btn-ouline-succes m-1">Login</a>
+    </div>
+  </div>
+</nav>
+<div class="container py-5">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body bg-light">
+                    <div class="text-center">
+                        <h5>Registrasi Account</h5>
+                    </div>
+                    <form action="config/aksi_register.php" method="POST">
+                        <label class="form-label">Username</label>
+                        <input type="text" name="username" class="form-control" ><br>
+                        <label class="form-label">Password</label><br>
+                        <input type="password" name="password" class="form-control">
+                        <label class="form-label">Email</label><br>
+                        <input type="email" name="email" class="form-control">
+                        <label class="form-label">Nama Lengkap</label><br>
+                        <input type="text" name="namalengkap" class="form-control">
+                        <label class="form-label">Alamat</label><br>
+                        <input type="text" name="alamat" class="form-control">
+                        <div class="d-grid mt-2">
+                            <button class="btn btn-primary" type="submit" name="kirim">Register</button>
+                        </div>
+                    </form>
+                    <hr>
+                    <p>Sudah punya akun? <a href="login.php">Login disini!</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="d-flex justify-content-center border-top mt-3 bg-light fixed-bottom">
+💙
+💙
+💙
+💙
+💙
+  <p>&copy; UKK RPL 2024 | Arnozzlea Ambarita</p>
+💙
+💙
+💙
+💙
+💙
+
+</footer>
+    <script type="text/javascript" src="asset/js/bootstrap.min.js"></script>
+</body>
+</html>
